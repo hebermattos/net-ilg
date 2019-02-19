82704703c1cf5bb36fc482c86aba31a0dacc732a
Heber Mattos <hebermattos@outlook.com>
2019-02-19 08:45:12 -0300

    nunit

--- App/ReportService.cs
+++ App/ReportService.cs
@@ -16,26 +16,29 @@ namespace App
 
         public void GenerateReport()
         {
-            var rawDatas = _dataRepository.GetData();
+            var rawReportDatas = _dataRepository.GetData();
 
             var processors = new List<ReportDataGenerator>
             {
                 new AmountClient()
             };
 
-            var reportData = new List<string>();
+            var processedReportDatas = new List<string>();
 
             foreach (var processor in processors)
             {
-                var data = processor.GetData(rawDatas);
+                var data = processor.GetData(rawReportDatas);
 
-                reportData.Add(data.Key + ": " + data.Value);
+                processedReportDatas.Add(data.Key + ": " + data.Value);
             }
 
-            var epoch = (int)(DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds;
+            System.IO.File.WriteAllLines(FolderPath.GetOutFolderPath() + "\\" + GenerateFileName() + ".done.data", processedReportDatas);
 
-            System.IO.File.WriteAllLines(FolderPath.GetOutFolderPath() + "\\report_" + epoch + ".done.data", reportData);
+        }
 
+        private static string GenerateFileName()
+        {
+            return "report_" + (int)(DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds;
         }
     }
 }
