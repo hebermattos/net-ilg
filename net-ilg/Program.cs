2ca9d992aa2670e13476057f698ac9f13a344fb6
Heber Mattos <hebermattos@outlook.com>
2019-02-19 17:10:54 -0300

    file watcher

--- net-ilg/Program.cs
+++ net-ilg/Program.cs
@@ -1,26 +1,40 @@
 ﻿using App;
 using Autofac;
+using Infra;
 using System;
+using System.IO;
 
 namespace net_ilg
 {
     class Program
-    {       
+    {
         static void Main(string[] args)
         {
             DependencyInjection.CreateContainer();
+
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = FolderPath.GetInFolderPath();
+
+            watcher.Filter = "*.dat";
+
+            watcher.Created += new FileSystemEventHandler(OnChanged);
+
+            watcher.EnableRaisingEvents = true;
+
+            Console.WriteLine("watching...");
+            Console.ReadLine();
+
+        }
+
+        private static void OnChanged(object source, FileSystemEventArgs e)
+        {
+            Console.WriteLine("New file to update report: " + e.FullPath);
 
             using (var scope = DependencyInjection.Container.BeginLifetimeScope())
             {
                 var reportService = scope.Resolve<ReportService>();
 
-                Console.WriteLine("generating report...");
-
                 reportService.GenerateReport();
-
-                Console.WriteLine("report generated successfully! press any key to continue ...");
-                Console.ReadLine();
-
             }
         }
     }
