2b90c9bdc38e5b0661b61b528558e7863b8d9d98
Heber Mattos <hebermattos@outlook.com>
2019-02-19 19:24:06 -0300

    foo

--- App/FolderService.cs
+++ App/FolderService.cs
@@ -1,11 +1,6 @@
 ﻿using Autofac;
 using Infra;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace App
 {
