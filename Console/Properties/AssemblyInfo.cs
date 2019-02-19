de2cd6f3edba7cf859c35b5328c9638d1faf13e6
Heber Mattos <hebermattos@outlook.com>
2019-02-19 20:48:53 -0300

    new console UI

--- Console/Properties/AssemblyInfo.cs
+++ Console/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("net-ilg")]
+[assembly: AssemblyTitle("Console")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("net-ilg")]
+[assembly: AssemblyProduct("Console")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("f86edb7f-fbfd-48cb-b451-d7e1e7e7afe0")]
+[assembly: Guid("f2a63bb8-3958-4956-a78e-531b0b81a36d")]
 
 // Version information for an assembly consists of the following four values:
 //
