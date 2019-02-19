c27d84a0dab1beb5fc630b370a0bc6c0b94cf8cb
Heber Mattos <hebermattos@outlook.com>
2019-02-19 09:16:47 -0300

    customer.cs

---
+++ Domain/Data/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Data
+{
+    class Customer
+    {
+        public string CNPJ { get; set; }
+        public string Name { get; set; }
+        public string BusinessArea { get; set; }
+
+        public Customer(string rawCustomer)
+        {
+            var customerData = rawCustomer.Split('ç');
+
+            if (customerData[0] != "002")            
+                throw new ArgumentException("Invalid customer data");            
+
+            CNPJ = customerData[1];
+            Name = customerData[2];
+            BusinessArea = customerData[3];
+        }
+    }
+}
