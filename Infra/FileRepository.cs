9d4a3fec2753dcc0fd4859e9a92f3159bcb2e64a
Heber Mattos <hebermattos@outlook.com>
2019-02-19 15:16:10 -0300

    bufferSize as seeting

--- Infra/FileRepository.cs
+++ Infra/FileRepository.cs
@@ -3,6 +3,8 @@ using System.Text;
 using System.IO;
 using System.Threading.Tasks;
 using System.Collections.Concurrent;
+using System.Configuration;
+using System;
 
 namespace Infra
 {
@@ -12,12 +14,12 @@ namespace Infra
         {
             var fullRawData = new ConcurrentBag<string>();
 
-            const int BufferSize = 32;
+            var bufferSize = Convert.ToInt32(ConfigurationManager.AppSettings["bufferSize"]);
 
             Parallel.ForEach(Directory.EnumerateFiles(FolderPath.GetInFolderPath(), "*.dat"), (file) =>
             {
                 using (var fileStream = File.OpenRead(file))
-                using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, BufferSize))
+                using (var streamReader = new StreamReader(fileStream, Encoding.UTF8, true, bufferSize))
                 {
                     string line;
                     while ((line = streamReader.ReadLine()) != null)
